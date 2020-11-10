2cf274ea4962f7f5e246ae41a6dfc6d7caad5ee9
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-10 10:50:48 +0200

    Initial Commit

---
+++ SwiftTipsPropertyWrappers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftTipsPropertyWrappers
+//
+//  Created by Ramill Ibragimov on 10.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftTipsPropertyWrappers/SwiftTipsPropertyWrappersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTipsPropertyWrappersApp.swift
+//  SwiftTipsPropertyWrappers
+//
+//  Created by Ramill Ibragimov on 10.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTipsPropertyWrappersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
